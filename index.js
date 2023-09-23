d40a676e730222abcddb29b50141daade2ab674e
Kevin Li <kevin.li128@gmail.com>
2023-09-22 19:30:49 -0700

    added cors

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const express = require('express');
+const cors = require('cors');
 
 require('dotenv').config();
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
 
-const api = require('./routes/api')
+const api = require('./routes/api');
 
-app.use('/api', api)
+app.use('/api', api);
 
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`)
-})
+  console.log(`Listening on port ${PORT}`);
+});
 
 module.exports = app;
