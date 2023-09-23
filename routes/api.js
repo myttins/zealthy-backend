39fb92e2a27817e8b9a773e15dea22435e80d463
Kevin Li <kevin.li128@gmail.com>
2023-09-23 10:12:39 -0700

    updated patch route

--- routes/api.js
+++ routes/api.js
@@ -15,23 +15,36 @@ router.get('/:id', async (req, res) => {
 });
 
 router.get('/', async (req, res) => {
-  const query = await db.query(`SELECT * FROM tickets`);
+  const query = await db.query(`SELECT * FROM tickets ORDER BY time DESC`);
   res.status(200).json(query.rows);
 });
 
 router.post('/', async (req, res) => {
   const { name, email, subject, message } = req.body;
-  const timestamp = new Date().toLocaleString();
+  const date = new Date();
 
   await db.query(`
-    INSERT INTO tickets (name, email, subject, message, timestamp)
-    VALUES ('${name}', '${email}', '${subject}', '${message}', '${timestamp}')`);
+    INSERT INTO tickets (name, email, subject, message, timestamp, time)
+    VALUES ('${name}', '${email}', '${subject}', '${message}', '${date.toLocaleString()}', ${
+    date.valueOf() - 1695000000000
+  })`);
 
-  res.status(200).json('post');
+  res.sendStatus(200);
 });
 
-router.patch('/', (req, res) => {
-  res.status(200).json('patch');
+router.patch('/:id', async (req, res) => {
+  const { id } = req.params;
+  const { status, response } = req.body;
+  const date = new Date();
+
+  await db.query(`
+  UPDATE tickets
+  SET status = '${status}', response = '${response}', timestamp = '${date.toLocaleString()}', time = ${
+    date.valueOf() - 1695000000000
+  }
+  WHERE id = ${id}`);
+
+  res.sendStatus(200);
 });
 
 module.exports = router;
